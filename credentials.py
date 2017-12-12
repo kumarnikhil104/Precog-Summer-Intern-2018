f1a6c2a77b0694adf76dfa85d8c24e4c6644022d
kumarnikhil104 <nikhilkumar_15508@aitpune.edu.in>
2017-12-12 14:16:08 +0530

    Updating Credentials

--- credentials.py
+++ credentials.py
@@ -1,3 +1,5 @@
+
+# Please don't abuse my credentials. Use the code for learning purpose only.
 access_token ='3074692450-CyWz6HSf1XTZGRaV9bRswDLwKcmoGoWZTlXAOD1'
 access_token_secret ='jiIU8pVrzeK0B8whx5YFJhvMwoHGHrWHZGbDI6SG9dJNf'
 
